2716005323770185a121e2e3090e6d23a2117b41
madvas <matus.lestan@gmail.com>
2015-06-30 22:25:47 +0200

    init commit

--- gulpfile.js
+++ gulpfile.js
@@ -71,10 +71,10 @@ gulp.task('jshint', function () {
 // Optimize Images
 gulp.task('images', function () {
   return gulp.src('app/images/**/*')
-    .pipe($.cache($.imagemin({
-      progressive: true,
-      interlaced: true
-    })))
+    //.pipe($.cache($.imagemin({
+    //  progressive: true,
+    //  interlaced: true
+    //})))
     .pipe(gulp.dest('dist/images'))
     .pipe($.size({title: 'images'}));
 });
